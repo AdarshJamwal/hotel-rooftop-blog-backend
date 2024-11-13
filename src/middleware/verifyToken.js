605b24e99236d13000eb8f581a809bf56886da6a
Adarsh Jamwal <adarshjamwalj19@gmail.com>
2024-11-13 21:19:13 +0530

    update verify Token.js

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
 if (!JWT_SECRET) {
@@ -8,25 +7,22 @@ if (!JWT_SECRET) {
 
 const verifyToken = (req, res, next) => {
   try {
-    // Try to retrieve the token from cookies or Authorization header
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
     if (!token) {
+      console.log("Token not provided in request");
       return res.status(401).json({ message: "No token provided" });
     }
 
     const decoded = jwt.verify(token, JWT_SECRET);
 
-    // Ensure the token contains a valid `userId`
     if (!decoded.userId) {
       return res.status(401).json({ message: "Invalid token provided" });
     }
 
-    // Attach decoded information to the request object
     req.userId = decoded.userId;
     req.role = decoded.role;
     next();
-
   } catch (error) {
     console.error("Error verifying token:", error);
     res.status(401).json({ message: "Invalid token" });
