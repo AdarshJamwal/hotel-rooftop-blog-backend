de9a716980cc1dbbffded05d0f0ef080da1cbf84
Adarsh Jamwal <adarshjamwalj19@gmail.com>
2024-11-13 23:06:23 +0530

    update genertateToken

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -6,7 +6,6 @@ const JWT_SECRET = process.env.JWT_SECRET_KEY;
 const verifyToken = (req, res, next)=>{
     try {
         const token = req.cookies.token;
-       //const token = req.headers.authorization?.split(' ')[1]; //Bearer Token
        if(!token){
         return res.status(401).send({message: "No Token provided"})
        }
